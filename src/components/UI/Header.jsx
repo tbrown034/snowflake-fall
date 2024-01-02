b9740502ac2536a3c4f7eb67ab90190dbcc5b3db
tbrown034 <tbrown034@gmail.com>
2024-01-01 21:04:38 -0500

    minor fixes

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header({ onToggleDarkMode, isDarkMode }) {
   return (
-    <header className={`flex justify-between items-center p-4 `}>
-      <div className="flex items-center gap-2 text-3xl">
+    <header className={`flex justify-between items-center  `}>
+      <div className="flex items-center gap-2 text-2xl">
         <i className="fas fa-snowflake"></i>
         <div>
           <span
@@ -22,13 +22,13 @@ export default function Header({ onToggleDarkMode, isDarkMode }) {
       </div>
       <button
         onClick={onToggleDarkMode}
-        className={`border p-1 text-xs rounded-lg ${
+        className={`border p-1 text-xs rounded-lg transition-colors duration-300 ${
           isDarkMode
-            ? "border-blue-200 hover:bg-blue-700 text-blue-200"
-            : "border-blue-900 hover:bg-blue-300 text-blue-900"
+            ? "border-blue-200 hover:bg-blue-200  hover:text-blue-900 text-blue-200"
+            : "border-blue-900 hover:bg-blue-900 hover:text-blue-200 text-blue-900"
         }`}
       >
-        {isDarkMode ? "Switch to Light" : "Switch to Dark"}
+        {isDarkMode ? "Light Mode" : "Dark Mode"}
       </button>
     </header>
   );
--- src/components/UI/Main.jsx
+++ src/components/UI/Main.jsx
@@ -2,6 +2,9 @@ export default function Main() {
   return (
     <main className="">
       <p>Main content and game instructions go here.</p>
+      <button className=" p-2  border-2  border-sky-50 rounded-lg dark:border-blue-900">
+        Start Game
+      </button>
     </main>
   );
 }
