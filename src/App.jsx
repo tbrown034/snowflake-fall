588ae37c0ec09b20d89c3a6649859fd18dab300c
tbrown034 <tbrown034@gmail.com>
2024-01-01 20:39:17 -0500

    feat: better UI /css

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,22 @@ import Main from "./components/UI/Main";
 import GameBoard from "./components/UI/GameBoard";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false); // State to manage dark mode
+  const [darkMode, setDarkMode] = useState(false);
 
-  // Function to toggle dark mode
   const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
+    setDarkMode((prevMode) => !prevMode); // Ensuring the state is toggled based on the previous state
   };
 
   return (
     <div
-      className={`min-h-screen p-4 flex flex-col justify-between ${
+      className={`min-h-screen p-4 flex flex-col justify-between transition-colors duration-300 ${
         darkMode ? "bg-blue-900 text-blue-200" : "bg-blue-200 text-blue-900"
-      } `}
+      }`}
     >
-      <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
-
+      <Header onToggleDarkMode={toggleDarkMode} isDarkMode={darkMode} />
       <Main />
-
       <GameBoard />
-      <Footer darkMode={darkMode} />
+      <Footer isDarkMode={darkMode} />
     </div>
   );
 }
--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,32 +1,36 @@
-export default function Header({ toggleDarkMode, darkMode }) {
+export default function Header({ onToggleDarkMode, isDarkMode }) {
   return (
     <header
-      className={`flex justify-between items-center p-4 ${
-        darkMode ? "bg-blue-800" : "bg-blue-100"
-      }`}
+      className={`flex justify-between items-center p-4 transition-colors duration-300 `}
     >
       <div className="flex items-center gap-2 text-3xl">
-        <i className="fas fa-snowflake"></i>{" "}
+        <i className="fas fa-snowflake"></i>
         <div>
           <span
-            className={`text-blue-900 ${darkMode ? "dark:text-blue-100" : ""}`}
+            className={`font-semibold ${
+              isDarkMode ? "text-blue-100" : "text-blue-900"
+            }`}
           >
             SnowFlake
           </span>
           <span
-            className={`text-blue-800 ${darkMode ? "dark:text-blue-200" : ""}`}
+            className={`font-light ${
+              isDarkMode ? "text-blue-200" : "text-blue-800"
+            }`}
           >
             Fall
           </span>
         </div>
       </div>
       <button
-        onClick={toggleDarkMode}
-        className={`border ${
-          darkMode ? "border-blue-200" : "border-blue-900"
-        } p-1 text-xs rounded-lg hover:bg-blue-300 dark:hover:bg-blue-700 transition-colors`}
+        onClick={onToggleDarkMode}
+        className={`border p-1 text-xs rounded-lg transition-colors duration-300 ${
+          isDarkMode
+            ? "border-blue-200 hover:bg-blue-700 text-blue-200"
+            : "border-blue-900 hover:bg-blue-300 text-blue-900"
+        }`}
       >
-        {darkMode ? "Light Mode" : "Dark Mode"}
+        {isDarkMode ? "Switch to Light" : "Switch to Dark"}
       </button>
     </header>
   );
